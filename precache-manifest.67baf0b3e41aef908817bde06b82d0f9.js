f878338829d658785eede168a74cb8f0017b92d6
Fagbuyi Oluwadurotimi <69780314+Durotimicodes@users.noreply.github.com>
2022-01-05 19:02:19 +0100

    Updates

---
+++ precache-manifest.67baf0b3e41aef908817bde06b82d0f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7639024e7b3efbbbc8d9",
+    "url": "/robofriends/static/css/main.55a06615.chunk.css"
+  },
+  {
+    "revision": "7639024e7b3efbbbc8d9",
+    "url": "/robofriends/static/js/main.7639024e.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/robofriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "18bcd20a95e582159e02",
+    "url": "/robofriends/static/css/2.ba9767b4.chunk.css"
+  },
+  {
+    "revision": "18bcd20a95e582159e02",
+    "url": "/robofriends/static/js/2.18bcd20a.chunk.js"
+  },
+  {
+    "revision": "d9078623073d367ccac9",
+    "url": "/robofriends/static/js/runtime~main.d9078623.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2f86850d4a04fc353d794a1999d26092",
+    "url": "/robofriends/index.html"
+  }
+];
